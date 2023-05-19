ec052ff615fa657fc0b49278e92ed9a9cc41c01a
vasileiosvyzas <vizasb@gmail.com>
2023-05-19 13:23:00 +0100

    Added a script to store some decorators used in the API

---
+++ src/api_decorators.py
@@ -0,0 +1,26 @@
+import pandas as pd
+
+from fastapi import FastAPI, Request
+from http import HTTPStatus
+from typing import Dict, List
+from datetime import datetime
+from functools import wraps
+
+def construct_response(f):
+    """Construct a JSON response for an endpoint."""
+
+    @wraps(f)
+    def wrap(request: Request, *args, **kwargs) -> Dict:
+        results = f(request, *args, **kwargs)
+        response = {
+            "message": results["message"],
+            "method": request.method,
+            "status-code": results["status-code"],
+            "timestamp": datetime.now().isoformat(),
+            "url": request.url._url,
+        }
+        if "data" in results:
+            response["data"] = results["data"]
+        return response
+
+    return wrap
