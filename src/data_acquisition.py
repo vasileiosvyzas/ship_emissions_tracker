b2537a4ffb5b8fbc969a9c56e592603db0606d17
vasileiosvyzas <vizasb@gmail.com>
2023-12-07 19:07:32 +0000

    Small refactoring changes and added logging to the script

--- src/data_acquisition.py
+++ src/data_acquisition.py
@@ -1,12 +1,37 @@
+import logging
+import os
 import time
 import traceback
 
+import boto3
 import pandas as pd
+from botocore.exceptions import ClientError
+from dotenv import find_dotenv, load_dotenv
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.wait import WebDriverWait
+
+# find .env automagically by walking up directories until it's found
+dotenv_path = find_dotenv()
+load_dotenv(dotenv_path)
+
+ACCESS_KEY_ID = os.environ.get("AWS_ACCESS_KEY_ID")
+SECRET_ACCESS_KEY = os.environ.get("AWD_SECRET_ACCESS_KEY")
+TABLE_NAME = os.environ.get("TABLE_NAME")
+DATABASE_NAME = os.environ.get("DATABASE_NAME")
+RESULT_OUTPUT_LOCATION = os.environ.get("RESULT_OUTPUT_LOCATION")
+
+logger = logging.getLogger(__name__)
+
+logger.setLevel(logging.INFO)
+
+# Create a file handler
+file_handler = logging.FileHandler("ship_emissions_tracker/logs/reports_updater.log")
+formatter = logging.Formatter("%(asctime)s - %(levelname)s - %(message)s")
+file_handler.setFormatter(formatter)
+logger.addHandler(file_handler)
 
 
 def get_reporting_table_content():
@@ -23,9 +48,9 @@ def get_reporting_table_content():
     driver = webdriver.Chrome(service=service, options=options)
 
     try:
-        driver.get("https://mrv.emsa.europa.eu/#public/emission-report")
+        logger.info("Visiting the Thetis MRV website")
 
-        time.sleep(60)
+        driver.get("https://mrv.emsa.europa.eu/#public/emission-report")
 
         WebDriverWait(driver, 10).until(
             EC.presence_of_element_located((By.XPATH, '//*[@id="exportablegrid-1137-body"]'))
@@ -34,37 +59,34 @@ def get_reporting_table_content():
         tables = driver.find_element(By.XPATH, '//*[@id="exportablegrid-1137-body"]')
         elements = tables.find_elements(By.TAG_NAME, "table")
 
-        keys = ["Reporting Period", "Version", "Generation Date", "File"]
         reports = []
+        keys = ["Reporting Period", "Version", "Generation Date", "File"]
+
         for table in elements:
-            print(table.text)
             result_dict = {keys[i]: value for i, value in enumerate(table.text.split("\n")[1:])}
             reports.append(result_dict)
-            print()
+
+        logger.info("Got the table data of the reports")
+        logger.info(reports.head())
 
         return pd.DataFrame(reports)
 
     except Exception as e:
-        print(f"An error occurred: {e}")
-        print(traceback.format_exc())
+        logger.error(f"An error occurred while getting the data: {e}")
+        logger.error(traceback.format_exc())
     finally:
         driver.quit()
 
 
-def download_new_file(report):
+def download_new_file(report, download_directory):
     """
     This function gets called to download the new file from the website
     It uses Selenium to click on the new link text
 
     """
-    print("Downloading the new file")
-
     service = Service()
     options = webdriver.ChromeOptions()
     options.add_argument("--headless=new")
-
-    # Specify the download directory
-    download_directory = "/Users/vasileiosvyzas/workspace/interview_exercises/CarbonChain/ship_emissions_tracker/data/raw"
     options.add_experimental_option("prefs", {"download.default_directory": download_directory})
 
     driver = webdriver.Chrome(service=service, options=options)
@@ -72,35 +94,40 @@ def download_new_file(report):
     time.sleep(10)
 
     try:
-        print(f"===={report}=====")
+        logger.info(f"Downloading the new report: {report}")
+
         wait = WebDriverWait(driver, 30)
         link = wait.until(EC.presence_of_element_located((By.LINK_TEXT, report)))
-        print("clicking the link")
         link.click()
+
         time.sleep(10)
 
     except Exception as e:
-        print(f"An error occurred: {e}")
-        print(traceback.format_exc())
+        logger.error(f"An error occurred while getting the data: {e}")
+        logger.error(traceback.format_exc())
     finally:
         driver.quit()
 
 
-def compare_versions(current_df, new_df):
+def compare_versions_and_download_file(current_df, new_df):
     """
     This function compares the version of the file from the new time we pull the data
     and it compares the new version with the old version that is on disk
     """
-    print("Comparing the two versions")
+    logger.info("Comparing the versions in the current and new dataframes")
 
     merged_df = pd.merge(
         current_df, new_df, on="Reporting Period", how="outer", suffixes=("_current", "_new")
     )
     new_versions = merged_df[merged_df["Version_new"] > merged_df["Version_current"]]
-    print("== New versions ==")
-    print(new_versions)
 
+    logger.info("Found the new versions")
+    logger.info(new_versions.head())
+
+    download_directory = "/Users/vasileiosvyzas/workspace/interview_exercises/CarbonChain/ship_emissions_tracker/data/raw"
     for index, row in new_versions.iterrows():
+        logger.info("Updating the current dataframe with the new versions")
+
         current_df.loc[current_df["Reporting Period"] == row["Reporting Period"], "Version"] = row[
             "Version_new"
         ]
@@ -111,15 +138,24 @@ def compare_versions(current_df, new_df):
             "File_new"
         ]
 
-        print(row["File_new"])
-        print(f"'{row['File_new']}'")
-        download_new_file(row["File_new"].strip())
+        download_new_file(report=row["File_new"].strip(), download_directory=download_directory)
 
-    print(current_df)
+        filepath = f"{download_directory}/{row['File_new'].strip()}.xlsx"
+        year = row["Reporting Period"]
+        filename = f"{row['File_new'].strip()}.xlsx"
+
+        upload_file(
+            file_name=filepath,
+            bucket="eu-marv-ship-emissions",
+            object_name=f"raw/{year}/{filename}",
+        )
+
     return current_df
 
 
 def fix_column_types(df):
+    logger.info("Convering the column types of the dataframe")
+
     df["Reporting Period"] = df[["Reporting Period"]].astype(int)
     df["Version"] = df[["Version"]].astype(int)
     df["Generation Date"] = pd.to_datetime(df["Generation Date"], dayfirst=True)
@@ -127,13 +163,37 @@ def fix_column_types(df):
     return df
 
 
-def update_table_data():
+def upload_file(file_name, bucket, object_name=None):
     """
-    This function updates the file on disk with the new version of the metadata
-    For example, if there is a new version of a file, this function should store these data
-    on the file
+    Upload a file to an S3 bucket
+
+    :param file_name: File to upload
+    :param bucket: Bucket to upload to
+    :param object_name: S3 object name. If not specified then file_name is used
+    :return: True if file was uploaded, else False
     """
-    pass
+
+    # If S3 object_name was not specified, use file_name
+    if object_name is None:
+        object_name = os.path.basename(file_name)
+
+    s3_client = boto3.client(
+        "s3",
+        region_name="us-east-1",
+        aws_access_key_id=ACCESS_KEY_ID,
+        aws_secret_access_key=SECRET_ACCESS_KEY,
+    )
+
+    try:
+        logger.info(
+            f"Uploading the file: {file_name} to the bucket: {bucket} with object name: {object_name}"
+        )
+
+        s3_client.upload_file(file_name, bucket, object_name)
+    except ClientError as e:
+        logging.error(e)
+        return False
+    return True
 
 
 def main():
@@ -146,7 +206,12 @@ def main():
     reports_df_old = fix_column_types(reports_df_old)
     print(reports_df_old.head())
 
-    reports_df_updated = compare_versions(current_df=reports_df_old, new_df=reports_df_new)
+    reports_df_updated = compare_versions_and_download_file(
+        current_df=reports_df_old, new_df=reports_df_new
+    )
+
+    logger.info("Got new files and added them to the S3 bucket")
+
     reports_df_updated.to_csv("ship_emissions_tracker/data/raw/reports_metadata.csv", index=False)
 
 
